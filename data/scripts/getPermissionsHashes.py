3b80d183944d582c2962a300ff32a7e6a55ec8fd
TheDevXen <thedevxen@gmail.com>
2018-08-23 02:53:31 +0000

    added admins

--- data/scripts/getPermissionsHashes.py
+++ data/scripts/getPermissionsHashes.py
@@ -1,3 +1,3 @@
-vipHashes = ['\xee\x80\xa0killer78887', '\xee\x80\xa0nancygamingcenter123']
-adminHashes = ['\xee\x80\xa0thedevxen','\xee\x80\xb0Linux2877','\xee\x80\xb0Android16575978']
+vipHashes = ['\xee\x80\xa0killer78887', '\xee\x80\xa0nancygamingcentre123']
+adminHashes = ['\xee\x80\xa0thedevxen','\xee\x80\xa0Laxator00','\xee\x80\xb0Linux2877','\xee\x80\xb0Android16575978','\xee\x80\xb0Android26953851']
 
